f9a6001d60b44eb710af2d50847b31be91352ed2
akakeishin <mizururi_22@gmail.com>
2016-06-13 12:41:19 +0900

    bluetooh test

---
+++ BTtest/BTtest.ino
@@ -0,0 +1,34 @@
+#include <SoftwareSerial.h>
+
+#define LED 13
+#define SRX 10
+#define STX 11
+
+
+void setup() {
+  pinMode(LED, OUTPUT);
+  Serial.begin(115200);
+
+  delay(50);
+  digitalWrite(LED,HIGH);
+  delay(1000);
+  digitalWrite(LED,LOW);
+  delay(1000);
+
+}
+
+void loop() {
+   char c;
+   if(Serial.available()){
+    c = Serial.read();
+
+    if(c == 'a'){
+      digitalWrite(LED,HIGH);
+    }
+    if(c == 'o'){
+      digitalWrite(LED,LOW);
+    }
+    delay(100);
+   }
+
+}
